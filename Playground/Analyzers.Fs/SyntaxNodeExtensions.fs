fcb95a6d26784a43ffd64f10509b633b85220e30
Costin Zaharia <cgzaharia@gmail.com>
2021-07-07 21:09:20 +0200

    Change project name

---
+++ Playground/Analyzers.Fs/SyntaxNodeExtensions.fs
@@ -0,0 +1,16 @@
+﻿module Analyzers.SyntaxNodeExtensions
+
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp.Syntax
+
+let getName(node: SyntaxNode) =
+    match node with
+    | :? MethodDeclarationSyntax as methodDeclaration -> methodDeclaration.Identifier.ValueText
+    | :? ClassDeclarationSyntax as classDeclaration -> classDeclaration.Identifier.ValueText
+    | :? TypeSyntax as typeSyntax -> typeSyntax.GetText().ToString()
+    | _ -> ""
+
+let isParent(node: SyntaxNode) =
+    match node with
+    | :? ClassDeclarationSyntax | :? StructDeclarationSyntax-> true
+    | _ -> false
