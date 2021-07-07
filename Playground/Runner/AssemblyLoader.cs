7e3e59c584730a7ceb60683167bbf1d01cee1d74
Costin Zaharia <cgzaharia@gmail.com>
2021-07-07 23:01:02 +0200

    Load solution

---
+++ Playground/Runner/AssemblyLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.CodeAnalysis;
+
+namespace Runner
+{
+    internal class AssemblyLoader : IAnalyzerAssemblyLoader
+    {
+        public static AssemblyLoader Instance = new AssemblyLoader();
+
+        public void AddDependencyLocation(string fullPath) => Assembly.LoadFrom(fullPath);
+
+        public Assembly LoadFromPath(string fullPath) => Assembly.LoadFrom(fullPath);
+    }
+}
---
+++ Playground/Runner/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Analyzers;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace Runner
+{
+    public static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var solutionPath = args[0];
+
+            RegisterMsBuild();
+            var workspace = await LoadSolution(solutionPath);
+            var analyzer = new AnalyzerFileReference(typeof(ExtensionsClassNameRule).Assembly.Location, AssemblyLoader.Instance);
+            var solution = workspace.CurrentSolution.AddAnalyzerReference(analyzer);
+
+            Console.WriteLine($"Solution loaded: {solution.FilePath}, Projects: {workspace.CurrentSolution.Projects.Count()}");
+        }
+
+        private static void RegisterMsBuild()
+        {
+            var msBuildInstance = MSBuildLocator.QueryVisualStudioInstances()
+                                                .OrderByDescending(x => x.Version)
+                                                .First();
+
+            MSBuildLocator.RegisterInstance(msBuildInstance);
+
+            Console.WriteLine($"MsBuild {msBuildInstance.Version} registered.");
+        }
+
+        private static async Task<MSBuildWorkspace> LoadSolution(string solutionPath)
+        {
+            var workspace = MSBuildWorkspace.Create();
+
+            await workspace.OpenSolutionAsync(solutionPath).ConfigureAwait(false);
+
+            foreach (var workspaceDiagnostic in GetFailures(workspace))
+            {
+                Console.WriteLine(workspaceDiagnostic.Message);
+            }
+
+            return workspace;
+        }
+
+        private static IEnumerable<WorkspaceDiagnostic> GetFailures(MSBuildWorkspace workspace) =>
+            workspace.Diagnostics
+                     .Where(workspaceDiagnostic => workspaceDiagnostic.Kind == WorkspaceDiagnosticKind.Failure);
+    }
+}
